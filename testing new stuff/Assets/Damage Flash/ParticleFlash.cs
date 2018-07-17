dbf66c1757e155f9686a99e578036da316339f8c
Unknown <genaralskar1@gmail.com>
2018-07-17 12:08:36 -0600

    reimport some packages, maybe fix it?

--- testing new stuff/Assets/Damage Flash/ParticleFlash.cs
+++ testing new stuff/Assets/Damage Flash/ParticleFlash.cs
@@ -6,36 +6,52 @@ public class ParticleFlash : MonoBehaviour
 {
 
 	public Material matOverride;
-	private Material startMat;
 	private Renderer mat;
 
 	public float flashTime = .1f;
 
 	public int flashNumber = 1;
 
+    public List<GameObject> flashObjects;
+
+    private Dictionary<Renderer, Material> matDic;
+
 	private void Start()
 	{
-		mat = GetComponent<Renderer>();
-		startMat = mat.material;
+        PopulateDictionary();
 	}
+
+    private void PopulateDictionary()
+    {
+        matDic = new Dictionary<Renderer, Material>();
+
+        foreach(GameObject obj in flashObjects)
+        {
+            matDic.Add(obj.GetComponent<Renderer>(), obj.GetComponent<Renderer>().material);
+        }
+    }
 
 	private void OnParticleCollision(GameObject other)
 	{
-		print("Flashing");
+	//	print("Flashing");
 		StopAllCoroutines();
 		StartCoroutine(Flash());
 	}
 
 	private IEnumerator Flash()
 	{
-
 		for (int i = 0; i < flashNumber; i++)
 		{
-			mat.material = matOverride;
+            foreach(GameObject obj in flashObjects)
+            {
+                obj.GetComponent<Renderer>().material = matOverride;
+            }
 			yield return new WaitForSeconds(flashTime);
-			mat.material = startMat;
+            foreach(GameObject obj in flashObjects)
+            {
+                obj.GetComponent<Renderer>().material = matDic[obj.GetComponent<Renderer>()];
+            }
 			yield return new WaitForSeconds(flashTime);	
-		}
-		
+		}	
 	}
 }
